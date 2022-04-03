baf6a646fe0de47609e09aa8faabd5edbd9c3632
HermanPlay <hermanplay1110@gmail.com>
2022-04-03 20:10:46 +0200

    tired already

--- bot copy.py
+++ bot copy.py
@@ -1,44 +1,50 @@
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-PORT = int(os.environ.get('PORT', 5000))
+import logging
+import telegram
+
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+
+import imaging
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+PORT = int(os.environ.get('PORT', 5000))
+ROOT = os.path.dirname(__file__)
 TOKEN = "5210758989:AAEhTnu8t5YrUB45zIJwDzngwesFsjwC4jc"
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
+
 def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    
+    update.message.reply_text('Hi! Send me text, which has to be put on the image')
 
 def help(update, context):
-    """Send a message when the command /help is issued."""
+    
     update.message.reply_text('Help!')
 
 def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    bot = telegram.Bot(token="5210758989:AAEhTnu8t5YrUB45zIJwDzngwesFsjwC4jc")
+    text_for_image = update.message.text
+    imaging.draw_image(text_for_image)
+    chatid = update.message.chat_id
+    bot.send_photo(chat_id=chatid, photo=open('Telegram-quote-bot//1.jpg', 'rb'))
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
 
-    # Get the dispatcher to register handlers
+def main():
+
+    updater = Updater("5210758989:AAEhTnu8t5YrUB45zIJwDzngwesFsjwC4jc", use_context=True)
+
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
 
@@ -58,6 +64,7 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
